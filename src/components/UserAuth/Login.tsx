83e7a6b0591092faafcf6332af1351e3a95d878b
Michael Chung <mikechung89@gmail.com>
2022-08-23 15:02:33 -0400

    added login component

---
+++ src/components/UserAuth/Login.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { UserAuth } from './model'
+
+const defaultState = {
+    username: '',
+    password: ''
+}
+
+const Login: React.FC = () => {
+    const [toggleShowPassword, setTogglePassword] = useState<boolean>(true);
+    const [formData, setFormData] = useState<UserAuth>(defaultState)
+    function handleSubmit(e : React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(formData)
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    function handleClick() {
+        setTogglePassword(!toggleShowPassword)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input onChange={handleChange} name='username' placeholder='Enter your username' value={formData.username}></input>
+            <label>Password</label>
+            <input onChange={handleChange} name='password' placeholder='Enter your password' type={toggleShowPassword ? "password" : "null"} value={formData.password}></input>
+            <button onClick={handleClick}> Show password</button>
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
+
+export default Login
